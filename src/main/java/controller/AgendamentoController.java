a04c2d45996c67b308e49aeb61bf5cad9b1fbd91
Sam Douglas Fransisco Dos Santos <samdouglask88@gmail.com>
2026-03-11 02:09:04 -0300

    commit

---
+++ src/main/java/controller/AgendamentoController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import com.kingsam.agendador_horarios.infrastrucutre.entity.AgendamentoEntity;
+import com.kingsam.agendador_horarios.service.AgendamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ClientHttpResponseDecorator;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+
+public class AgendamentoController {
+    private final AgendamentoService agendamentoService;
+
+    @PatchMapping
+    public ResponseEntity<AgendamentoEntity> salvarAgedanebti(@RequestBody AgendamentoEntity agendamento) {
+        return ResponseEntity.ok().body(agendamentoService.salvarAgendamento(agendamento));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarAgendamento(@RequestBody AgendamentoEntity agendamento) {
+        agendamentoService.deletarAgendamento(agendamento.getDataHoraAgendamento(), agendamento.getCliente());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<AgendamentoEntity> buscarAgendamentoDia(@RequestBody AgendamentoEntity agendamento) {
+        return ResponseEntity.ok().body(agendamentoService.buscarAgentamentoDia(agendamento.getDataHoraAgendamento(), agendamento.getDataHoraAgendamento()));
+    }
+
+    @PutMapping
+    public ResponseEntity<AgendamentoEntity> alterarAgendamento(@RequestBody AgendamentoEntity agendamento)  {
+        return ResponseEntity.ok().body(agendamentoService.alterarAgendamento(agendamento, agendamento.getCliente(), agendamento.getDataHoraAgendamento()));
+    }
+}
+
+
